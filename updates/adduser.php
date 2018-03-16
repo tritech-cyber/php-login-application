1659d9212141831d4760944b8b55dd990b33b5bb
Tri-Tech <craig.coleman@ksd.org>
2018-03-16 05:34:47 -0700

    Add files via upload

---
+++ updates/adduser.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+require_once('class.user.php');
+$user = new USER();
+
+if($user->is_loggedin()!="")
+{
+	$user->redirect('home.php');
+}
+
+if(isset($_POST['btn-signup']))
+{
+	$uname = strip_tags($_POST['txt_uname']);
+	$umail = strip_tags($_POST['txt_umail']);
+	$upass = strip_tags($_POST['txt_upass']);
+	$check = strip_tags($_POST['txt_check']);
+	
+	if($upass != $check ){
+		$error[] = "passwords do not match !";	
+	}
+	else if($uname=="")	{
+		$error[] = "provide username !";	
+	}
+	else if($umail=="")	{
+		$error[] = "provide email id !";	
+	}
+	else if(!filter_var($umail, FILTER_VALIDATE_EMAIL))	{
+	    $error[] = 'Please enter a valid email address !';
+	}
+	else if($upass=="")	{
+		$error[] = "provide password !";
+	}
+	else if(strlen($upass) < 6){
+		$error[] = "Password must be atleast 6 characters";	
+	}
+	else
+	{
+		try
+		{
+			$stmt = $user->runQuery("SELECT user_name, user_email FROM users WHERE user_name=:uname OR user_email=:umail");
+			$stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
+			$row=$stmt->fetch(PDO::FETCH_ASSOC);
+				
+			if($row['user_name']==$uname) {
+				$error[] = "sorry username already taken !";
+			}
+			else if($row['user_email']==$umail) {
+				$error[] = "sorry email id already taken !";
+			}
+			else
+			{
+				if($user->register($uname,$umail,$upass)){	
+					$user->redirect('adduser.php?joined');
+				}
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}	
+}
+
+?>
+<html>
+<head>
+<title>Add User</title>
+</head>
+<body>
+
+    	
+        <form method="post" >
+            <h2 >Add user</h2><hr />
+            <?php
+			if(isset($error))
+			{
+			 	foreach($error as $error)
+			 	{
+					 echo $error; 
+				}
+			}
+			else if(isset($_GET['joined']))
+			{
+				 ?>
+                  Successfully registered <a href='index.php'>login</a>
+                 <?php
+			}
+			?>
+   
+            <input type="text"  name="txt_uname" placeholder="Enter Username" value="<?php if(isset($error)){echo $uname;}?>" />
+            <input type="text"  name="txt_umail" placeholder="Enter E-Mail ID" value="<?php if(isset($error)){echo $umail;}?>" />
+			<input type="password"  name="txt_upass" placeholder="Enter Password" />
+			<input type="password"  name="txt_check" placeholder="Re-Enter Password" />
+            	<button type="submit" name="btn-signup">
+                &nbsp;SIGN UP
+                </button>
+            <br />
+            <label>have an account ! <a href="index.php">Sign In</a></label>
+        </form>
+<hr />
+<pre>
+
+</pre>
+</body>
+</html>
