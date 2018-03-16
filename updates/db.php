d2d0828f4c478efe1e8267a50fa0034eb3b47719
Tri-Tech <craig.coleman@ksd.org>
2018-03-16 06:34:30 -0700

    Add files via upload

---
+++ updates/db.php
@@ -0,0 +1,27 @@
+<?php
+class Database
+{   
+    private $hostname = "localhost";
+    private $dbname = "stream";
+    private $username = "zoey";
+    private $password = "redhat2001";
+    public $conn;
+     
+    public function dbConnection()
+	{
+     
+	    $this->conn = null;    
+        try
+		{
+            $this->conn = new PDO("mysql:hostname=".$this->hostname . ";dbname=" . $this->dbname, $this->username, $this->password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+        }
+		catch(PDOException $exception)
+		{
+            echo "Connection error: " . $exception->getMessage();
+        }
+         
+        return $this->conn;
+    }
+}
+?>
